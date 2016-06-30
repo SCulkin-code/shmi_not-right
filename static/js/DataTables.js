6ece3de1695ccc793e19cd81f06fc36b9537f866
sassbril <sjculkin@gmail.com>
2016-06-30 10:25:20 +0100

    Add files via upload

---
+++ static/js/DataTables.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#example').dataTable({"sPaginationType": "full_numbers"});
+});
+
+$(document).ready(function() {
+    $('#table').dataTable({"sPaginationType": "full_numbers"});
+});
